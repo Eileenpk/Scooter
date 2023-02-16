d8fe312da884959de6b94b7750921710ac1810ee
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-02-16 16:39:36 -0500

    setup: Header in all pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import Header from '@/components/Header'
 export default function Home() {
   return (
     <>
@@ -11,7 +10,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
       <main className={styles.main}>
         <h1>testing</h1>
       </main>
