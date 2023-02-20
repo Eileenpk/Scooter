be2c288b80ec8c8f6f98297f6c1cbc34394740ee
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-02-20 17:05:19 -0500

    finish:tablet CSS

--- components/Hero.js
+++ components/Hero.js
@@ -1,26 +1,51 @@
 import styles from "@/styles/hero.module.css";
 import Image from "next/image";
+import GetWindowWidth from "@/components/functions/GetWindowWidth";
 import arrowImg from "@/public/patterns/right-arrow.svg";
-import arrowImgMobile from "@/public/patterns/right-arrow.png"
+import arrowImgMobile from "@/public/patterns/right-arrow.png";
+import circles from "@/public/patterns/white-circles.svg";
+
+
 export default function Hero({ title, text, btn, src, height, arrow, plain }) {
+  const { width } = GetWindowWidth();
+
   const heroStyles = {
     backgroundImage: `url(${src})`,
     height: height,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
   };
+
   return (
     <section className={styles.hero} style={heroStyles}>
-      <h1 className={plain ? styles.heroHeadingOnly : styles.heroHeading }>{title}</h1>
+      <h1 className={plain ? styles.heroHeadingOnly : styles.heroHeading}>
+        {title}
+      </h1>
       <p className={styles.heroText}>{text}</p>
       <div className={styles.heroBtn}>{btn}</div>
-      {arrow && (
+      {arrow && width < 768 && (
         <Image
           src={arrowImgMobile}
-        //   width={447}
-        //   height={138}
+          alt="arrow pointing right"
           className={styles.heroArrow}
         />
+      )}
+
+      {arrow && width > 768 && (
+        <div className={styles.heroImageContainer}>
+          <Image
+            src={arrowImg}
+            alt="arrow pointing right"
+            className={styles.heroArrow}
+          />
+
+          <Image 
+          src={circles}
+          alt='three white circles in a row'
+          className={styles.heroCircles}
+          width={234}
+          />
+        </div>
       )}
     </section>
   );
