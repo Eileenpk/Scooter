369f10ddba305c119d84bff1aa75cb4f549bc03b
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-02-21 13:58:19 -0500

    finish: Feature tablet CSS

--- components/Feature.js
+++ components/Feature.js
@@ -1,16 +1,22 @@
 import styles from "@/styles/feature.module.css";
 import Image from "next/image";
+import GetWindowWidth from "@/components/functions/GetWindowWidth";
 export default function Feature(featureInfo) {
-
+  const { width } = GetWindowWidth();
+ 
   const featuresmap = featureInfo.featureInfo.map((card) => {
     return (
       <section className={styles.feature} key={card.heading}>
-        <div className={card.reverse ? styles.reverse : styles.featureImageContainer}>
+        <div
+          className={
+            card.reverse ? styles.reverse : styles.featureImageContainer
+          }
+        >
           <Image
             src={card.image}
             alt={card.heading}
-            width={311}
-            height={311}
+            width={width > 768 ? 445 :311}
+            height={width > 768 ? 445 :311}
             className={styles.featureMainImage}
           />
         </div>
